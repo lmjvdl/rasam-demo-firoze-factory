d3e505a8b34c601ebdde670b4b9b28232a90c1b4
MohammadJavad HosseinPoor <lmjvdl82@gmail.com>
2025-03-02 19:44:47 +0330

    Initial changes

---
+++ src/app/admin/user/AllContent.tsx
@@ -0,0 +1,146 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import getCompanyList, { ResponseSchema } from "./useView";
+import useDelete from "./useDelete";
+import useUpdate from "./useUpdate";
+import CompanyTable from "./CompanyTable";
+import ViewDialog from "@/components/AdminPanelComponent/ViewProcess/ViewDialog";
+import EditDialog from "@/components/AdminPanelComponent/ViewProcess/EditDialog";
+import DeleteDialog from "@/components/AdminPanelComponent/ViewProcess/DeleteDialog";
+import ViewUserModalDialog from "@/app/admin/company/ViewUserModal";
+import { PrevDataInitial } from "@/interfaces/general/general";
+import { columns } from "./ColumnsData";
+
+const AllContentCompany: React.FC = () => {
+  const [data, setData] = useState<ResponseSchema>(PrevDataInitial);
+  const [selectedRow, setSelectedRow] = useState<any>(null);
+  const [selectedCompanyId, setSelectedCompanyId] = useState<number | null>(
+    null
+  );
+  const [viewOpen, setViewOpen] = useState(false);
+  const [deleteOpen, setDeleteOpen] = useState(false);
+  const [editOpen, setEditOpen] = useState(false);
+  const [viewUsersOpen, setViewUsersOpen] = useState(false);
+  const [pageNumber, setPageNumber] = useState<number>(0);
+  const { deleteCompanyMutation } = useDelete();
+  const { updateCompanyMutation } = useUpdate();
+  const [totalData, setTotalData] = useState<number>(0);
+  const [nextPage, setNextPage] = useState<null | string>(null);
+
+  const getList = getCompanyList(pageNumber, 8, nextPage);
+
+  useEffect(() => {
+    getList.mutate(
+      { page: pageNumber + 1, page_size: 8, url: nextPage },
+      {
+        onSuccess: (information) => {
+          setData(information);
+          setTotalData(information.data.count);
+          setNextPage(information.data.next);
+        },
+      }
+    );
+  }, [pageNumber]);
+
+  const handleSaveEdit = (updatedRow: {
+    id: number;
+    name: string;
+    description: string;
+    code: string;
+    logo: string;
+  }) => {
+    setData((prevData) => {
+      if (prevData?.data) {
+        updateCompanyMutation.mutate(updatedRow);
+        return {
+          ...prevData,
+          data: {
+            ...prevData.data,
+            results: prevData.data.results.map((row) =>
+              row.id === updatedRow.id ? updatedRow : row
+            ),
+          },
+        };
+      }
+      return prevData || PrevDataInitial;
+    });
+  };
+
+  const handlePagination = (newPage: number) => {
+    setPageNumber(newPage);
+    getList.mutate({ page: newPage + 1, page_size: 8, url: nextPage });
+  };
+
+  const handleUsersView = (companyId: number) => {
+    setSelectedCompanyId(companyId);
+    setViewUsersOpen(true);
+  };
+
+  const handleView = (row: any) => {
+    setSelectedRow(row);
+    setViewOpen(true);
+  };
+
+  const handleEdit = (row: any) => {
+    setSelectedRow(row);
+    setEditOpen(true);
+  };
+
+  const handleDelete = (row: any) => {
+    setSelectedRow(row);
+    setDeleteOpen(true);
+  };
+
+  const handleConfirmDelete = () => {
+    if (selectedRow?.id) {
+      deleteCompanyMutation.mutate(selectedRow.id);
+      setDeleteOpen(false);
+    }
+  };
+
+  const dynamicColumns = columns();
+  const filteredComumnsForEdit = dynamicColumns.filter((col) => col.canEdit);
+
+  return (
+    <>
+      <CompanyTable
+        data={data?.data?.results ?? []}
+        columns={dynamicColumns}
+        onView={handleView}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
+        handleUsersView={handleUsersView}
+        selectedCompanyId={selectedCompanyId}
+        userList={userList}
+        viewUsersOpen={viewUsersOpen}
+        page={pageNumber}
+        count={totalData}
+        onPageChange={handlePagination}
+      />
+
+      <ViewDialog
+        open={viewOpen}
+        onClose={() => setViewOpen(false)}
+        rowData={selectedRow}
+        titles={dynamicColumns}
+      />
+      <EditDialog
+        open={editOpen}
+        onClose={() => setEditOpen(false)}
+        onSave={handleSaveEdit}
+        rowData={selectedRow}
+        titles={filteredComumnsForEdit}
+      />
+      <DeleteDialog
+        open={deleteOpen}
+        onClose={() => setDeleteOpen(false)}
+        onConfirm={handleConfirmDelete}
+        rowData={selectedRow}
+        titles={dynamicColumns}
+      />
+    </>
+  );
+};
+
+export default AllContentCompany;
