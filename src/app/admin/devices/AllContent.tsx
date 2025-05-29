4379efdd843a42d84546d5da7376a7dc53006d07
MohammadJavad HosseinPoor <lmjvdl82@gmail.com>
2025-05-29 13:42:20 +0330

    Change and debug devices part

--- src/app/admin/devices/AllContent.tsx
+++ src/app/admin/devices/AllContent.tsx
@@ -11,14 +11,12 @@ import { columns } from "./ColumnsData";
 import { DeviceUpdateSchema } from "./hooks/useUpdate";
 import DeviceTable from "./DeviceTable";
 import useUpdate from "./hooks/useUpdate";
-import useDataQuery from "@/hooks/adminDataQuery/useDataQuery";
-import allQueryKeys from "@/utils/dataFetching/allQueryKeys";
-import dataTypeUrls from "@/utils/url/adminPanel/dataTypeUrl";
-import productLinePartUrls from "@/utils/url/adminPanel/productLinePartUrl";
+import { Device } from "@/interfaces/admin/device";
+import { useDeviceExtraOptions } from "./hooks/useDeviceExtraOptions";
 
 const AllContentDevice: React.FC = () => {
   const [data, setData] = useState<ResponseSchema>(PrevDataInitial);
-  const [selectedRow, setSelectedRow] = useState<any>(null);
+  const [selectedRow, setSelectedRow] = useState<Device>();
   const [viewOpen, setViewOpen] = useState(false);
   const [deleteOpen, setDeleteOpen] = useState(false);
   const [editOpen, setEditOpen] = useState(false);
@@ -26,37 +24,11 @@ const AllContentDevice: React.FC = () => {
   const [totalData, setTotalData] = useState<number>(0);
   const [nextPage, setNextPage] = useState<null | string>(null);
 
+  const { dataTypeList, productLinePartList } = useDeviceExtraOptions();
+
   const getList = getDeviceList(pageNumber, 8, nextPage);
   const { deleteDeviceMutation } = useDelete();
   const { updateDeviceMutation } = useUpdate();
-
-  const dataTypeList = useDataQuery(
-    allQueryKeys.adminPanel.devices.data_type_list,
-    dataTypeUrls.listDataType
-  ).data
-    ? useDataQuery(
-        allQueryKeys.adminPanel.devices.data_type_list,
-        dataTypeUrls.listDataType
-      ).data.map((device) => ({
-        id: device.id,
-        value: device.id,
-        label: device.name,
-      }))
-    : [];
-
-  const productLinePartList = useDataQuery(
-    allQueryKeys.adminPanel.devices.product_line_part_list,
-    productLinePartUrls.listProductLinePart
-  ).data
-    ? useDataQuery(
-        allQueryKeys.adminPanel.devices.product_line_part_list,
-        productLinePartUrls.listProductLinePart
-      ).data.map((product_line_part) => ({
-        id: product_line_part.id,
-        value: product_line_part.id,
-        label: product_line_part.name,
-      }))
-    : [];
 
   useEffect(() => {
     getList.mutate(
@@ -95,17 +67,17 @@ const AllContentDevice: React.FC = () => {
     getList.mutate({ page: newPage + 1, page_size: 8, url: nextPage });
   };
 
-  const handleView = (row: any) => {
+  const handleView = (row: Device) => {
     setSelectedRow(row);
     setViewOpen(true);
   };
 
-  const handleEdit = (row: any) => {
+  const handleEdit = (row: Device) => {
     setSelectedRow(row);
     setEditOpen(true);
   };
 
-  const handleDelete = (row: any) => {
+  const handleDelete = (row: Device) => {
     setSelectedRow(row);
     setDeleteOpen(true);
   };
@@ -176,7 +148,6 @@ const AllContentDevice: React.FC = () => {
         rowData={selectedRow}
         titles={dynamicColumns}
         arrayAttributes={{ data_type: "name" }}
-        objectAttributes={["product_line_part", "on_off_identifier"]}
       />
     </>
   );
--- src/app/admin/devices/page.tsx
+++ src/app/admin/devices/page.tsx
@@ -5,21 +5,11 @@ import ModalForm from "@/components/adminPanelComponent/addingProcess/ModalForm"
 import MainCard from "@/components/customContiner/MainCard";
 import { createNewDevice } from "./hooks/useCreate";
 import AllContentDevice from "./AllContent";
-import useDataQuery from "@/hooks/adminDataQuery/useDataQuery";
-import allQueryKeys from "@/utils/dataFetching/allQueryKeys";
-import productLinePartUrls from "@/utils/url/adminPanel/productLinePartUrl";
-import dataTypeUrls from "@/utils/url/adminPanel/dataTypeUrl";
+import { useDeviceExtraOptions } from "./hooks/useDeviceExtraOptions";
 
 export default function DevicePage() {
   const [refreshKey, setRefreshKey] = useState(0);
-  const getListProductLinePart = useDataQuery(
-    allQueryKeys.adminPanel.devices.product_line_part_list,
-    productLinePartUrls.listProductLinePart
-  );
-  const getListDataType = useDataQuery(
-    allQueryKeys.adminPanel.devices.data_type_list,
-    dataTypeUrls.listDataType
-  );
+  const { productLinePartOptions, dataTypeOptions } = useDeviceExtraOptions();
 
   const handleCreateDevice = async (data: unknown) => {
     const response = await createNewDevice(data);
@@ -52,33 +42,21 @@ export default function DevicePage() {
             label: "خط تولید جزئی",
             type: "select",
             required: true,
-            options:
-              getListProductLinePart.data?.map((product_line_part) => ({
-                label: product_line_part.name,
-                value: product_line_part.id,
-              })) || [],
+            options: productLinePartOptions,
           },
           {
             name: "data_type",
             label: "نوع داده",
             type: "multiselect",
             required: true,
-            options:
-              getListDataType.data?.map((data_type) => ({
-                label: data_type.name,
-                value: data_type.id,
-              })) || [],
+            options: dataTypeOptions,
           },
           {
             name: "on_off_identifier",
             label: "مشخص کننده خاموشی",
             type: "select",
             required: true,
-            options:
-              getListDataType.data?.map((data_type) => ({
-                label: data_type.name,
-                value: data_type.id,
-              })) || [],
+            options: dataTypeOptions,
           },
           {
             name: "value",
